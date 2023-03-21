bd14690416ff01bf79ed4926a8778c69151bb7ca
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-03-21 21:44:58 +0100

    variable olusturma

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day02_dataTurleri_Scanner/C01_VariableOlusturmaveKullanma.java
@@ -0,0 +1,23 @@
+package day02_dataTurleri_Scanner;
+
+public class C01_VariableOlusturmaveKullanma {
+
+    public static void main(String[] args) {
+
+        int sayi;
+
+        sayi=20;
+        sayi=30;
+        sayi=40;
+
+        System.out.println(sayi);
+
+        System.out.println("sayi");
+
+        sayi=50;
+        sayi=sayi*2+20;
+        System.out.println(sayi);
+        System.out.println("sayi = " + sayi);
+
+    }
+}
