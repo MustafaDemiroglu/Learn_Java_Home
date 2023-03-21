41fe7319d624eb656332be946d029312cd50527a
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-03-21 21:50:21 +0100

    Data türleri

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day02_dataTurleri_Scanner/C02_DataTurleri.java
@@ -0,0 +1,12 @@
+package day02_dataTurleri_Scanner;
+
+public class C02_DataTurleri {
+
+    public static void main(String[] args) {
+
+        char ilkHarf= '8' // Tek tirnak icerisinde tek karakter ekleyerek yazabiliriz sadece
+
+
+
+    }
+}
