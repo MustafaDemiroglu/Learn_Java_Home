79ac1480a8c40f976e6c39041571f6e33c5cd744
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-03-21 21:35:46 +0100

    java variables

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day02_dataTurleri_Scanner/C01_VariableOlusutmaveKullanma.java
@@ -0,0 +1,17 @@
+package day02_dataTurleri_Scanner;
+
+public class C01_VariableOlusutmaveKullanma {
+    public static void main(String[] args) {
+
+        int sayi ;
+
+        sayi = 20 ;
+
+        System.out.println(sayi); // 20
+
+        System.out.println("sayi"); /* sayi */
+
+
+
+    }
+}
