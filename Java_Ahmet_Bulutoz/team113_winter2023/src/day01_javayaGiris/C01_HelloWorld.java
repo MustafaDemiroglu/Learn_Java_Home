ffd124e490dc62ce1e1b230a370867375ef3d926
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-03-21 21:20:25 +0100

    java neu

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day01_javayaGiris/C01_HelloWorld.java
@@ -0,0 +1,32 @@
+package day01_javayaGiris;
+
+public class C01_HelloWorld {
+
+    public static void main(String[] args) {
+       // sout yauip entera basarsaniz konsolda yazdirma kodu oln SYstem.out.print.in() cikar
+       // java aciklama satirlarini görmezden gelir
+       //intellij de bu aciklama yazilarini gri renkli yapar
+        System.out.println("Hello World");
+
+        /*
+        Bu sekilde
+        yapinca
+        birden fazla satir
+        yorum satiriolur
+         */
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day01_javayaGiris/C02_Variables.java
@@ -0,0 +1,23 @@
+package day01_javayaGiris;
+
+public class C02_Variables {
+    public static void main(String[] args) {
+        /*
+            java runtime programdir
+            yani biuz yesil oka basip calistirdihgimizda hersey sifirdan baslar
+            class in sonuna kadar tüm kodalr yukaridan asagi calistirilir
+            class bittiginde hersey sifirdan baslar
+         */
+
+        int bakiye;
+
+        bakiye=500;
+
+        bakiye=200;
+
+        System.out.println(bakiye); //200
+        bakiye=300;
+
+        System.out.println(bakiye); //300
+    }
+}
