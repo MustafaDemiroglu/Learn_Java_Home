3c6eb96f01a81e8abb893f413d30f54008c97c46
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-04-05 18:14:31 +0200

    new project

---
+++ Coursera_DukeUni_Java/with Intellij/Coursera_DukeUni_JavaProgramming/src/D03_loopSyntax/C01_javaWhileLoop.java
@@ -0,0 +1,12 @@
+package D03_loopSyntax;
+
+public class C01_javaWhileLoop {
+    public static void main(String[] args) {
+        int x = 4 ;
+        int y = 1 ;
+        while (x>y) {
+            System.out.println("Hello");
+            x = x-1 ;
+    }
+}
+}
