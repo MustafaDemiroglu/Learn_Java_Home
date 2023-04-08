1ddf22da28ac30faf3e60e77c6abcdb3779a8998
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-04-08 14:46:48 +0200

    Allcodons

---
+++ Coursera_DukeUni_Java/with Bluej/StringsFirstAssignments/AllCodons.java
@@ -0,0 +1,14 @@
+
+/**
+ * Beschreiben Sie hier die Klasse AllCodons.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class AllCodons {
+    
+    public int findStopCodon(String dnaStr, int startIndex, String stopCodon){
+        
+    }   
+    
+}
