f68d5ad6cd435e46447e2a2e6cadf9148879f472
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-04-08 14:51:18 +0200

    dna codon

--- Coursera_DukeUni_Java/with Bluej/StringsFirstAssignments/AllCodons.java
+++ Coursera_DukeUni_Java/with Bluej/StringsFirstAssignments/AllCodons.java
@@ -8,7 +8,15 @@
 public class AllCodons {
     
     public int findStopCodon(String dnaStr, int startIndex, String stopCodon){
-        
+        //find stopCodon stating from (startIndex+3), currIndex
+        //as long as currIndex is not equal to -1
+            //check if currIndex - startIndex is a multiple of 3
+            //if so, currIndex is answer, return it
+            //if not, update currIndex, looking for stopCodon
+            //starting from currIndex + 1
+        //if we exit loop, we didn't find stopCodon
+        //so return dnaStr.length()
+        return 0;
     }   
     
 }
