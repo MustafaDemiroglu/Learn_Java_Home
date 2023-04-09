d813c60fb8f3ca5d2c72f18858ac3b634afb8beb
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-04-09 18:27:31 +0200

    Update AllCodons.java

--- Coursera_DukeUni_Java/with Bluej/StringsFirstAssignments/AllCodons.java
+++ Coursera_DukeUni_Java/with Bluej/StringsFirstAssignments/AllCodons.java
@@ -2,22 +2,30 @@
 /**
  * Beschreiben Sie hier die Klasse AllCodons.
  * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
+ * @author (Mustafa Demiroglu) 
+ * @version (09.04.2023)
  */
 public class AllCodons {
     
     public int findStopCodon(String dnaStr, int startIndex, String stopCodon){
-        //find stopCodon stating from (startIndex+3), currIndex
-            int currIndex = dna.indexOf(stopCodon, startIndex);
-        //as long as currIndex is not equal to -1
-            //check if currIndex - startIndex is a multiple of 3
-            //if so, currIndex is answer, return it
-            //if not, update currIndex, looking for stopCodon again
-            //  starting from currIndex + 1
+        //find stopCodon starting from (startIndex+3), currIndex
+            int currIndex = dnaStr.indexOf(stopCodon, startIndex);
+         //as long as currIndex is not equal to -1
+            while (currIndex != -1) {
+                int diff = currIndex - startIndex;
+                //check if currIndex - startIndex is a multiple of 3
+                if (diff % 3 == 0) {
+                     //if so, currIndex is answer, return it
+                    return currIndex;
+                    //if not, update currIndex, looking for stopCodon again
+                } else {
+                    //  starting from currIndex + 1
+                    currIndex = dnaStr.indexOf(stopCodon, currIndex+1);
+                }
         //if we exit loop, we didn't find stopCodon
         //so return dnaStr.length()
-        return 0;
+        }
+        return dnaStr.length();
     }
     
     public String findGene(String dna){
@@ -30,5 +38,19 @@ public class AllCodons {
         //if minIndex is dna.length()? nothing found, return""
         //otherwise answer is text from startIndex to minIndex
         return"";
+    }
+    
+    public void testFindStop () {
+        //            01234567890123456789012345 
+        String dna = "xxxyyyzzzTAAxxxyyyzzzTAAxx";
+        int dex = findStopCodon (dna, 0, "TAA");
+        if (dex != 9) System.out.println ("error on 9");
+        dex = findStopCodon (dna, 9, "TAA");
+        if (dex != 21) System.out.pritln ("error on 21");
+        dex = findStopCodon (dna,1,"TAA");
+        if (dex != 26) System.out.println ("error on 26");
+        dex = findStopCodon (dna, 0, "TAG");
+        if (dex != 26) System.out.println ("error on 26 TAG");
+        System.out.println ("tests finished and no Fehler found");
     }
 }
