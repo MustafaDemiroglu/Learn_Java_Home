710045dd4644eda8d6804fc602f27e704a4df7ad
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-05-15 22:24:51 +0200

    Update C01_AsciiTable.java

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day04_dataCasting_wrapperClasses/C01_AsciiTable.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day04_dataCasting_wrapperClasses/C01_AsciiTable.java
@@ -1,4 +1,12 @@
 package day04_dataCasting_wrapperClasses;
 
 public class C01_AsciiTable {
+
+    public static void main(String[] args) {
+
+        /*
+         * 24 ==> 11000
+         * a ==>
+         */
+    }
 }
