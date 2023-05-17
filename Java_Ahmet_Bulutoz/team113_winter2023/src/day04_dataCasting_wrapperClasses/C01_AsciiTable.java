8ae41fae753e076032b68e6d0d868b3be70c62fb
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-05-17 22:37:27 +0200

    revision

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day04_dataCasting_wrapperClasses/C01_AsciiTable.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day04_dataCasting_wrapperClasses/C01_AsciiTable.java
@@ -9,6 +9,15 @@ public class C01_AsciiTable {
          * a ==> 97 ==> 10100010
          * b ==> 98
          * ahmet icin ise her bir karakter ayri ayri makine diline cevrilir
+         * char data türündeki variable ve degerler
+         * sayisal data türlerine atanirsa veya
+         * sayisal data türündeki datalarla isleme sokulursa
+         * ASCII tabledaki degerleri ile isleme girer
          */
+
+        System.out.println(5+'a');   // 102 sonucunu verir
+
+        char ilkharf = 101;
+        System.out.println(ilkharf);
     }
 }
