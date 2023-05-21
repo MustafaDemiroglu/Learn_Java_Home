c530ea4cd5bf6a93f512a890e6629301ce5f0cdc
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-05-21 22:08:57 +0200

    ascii

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day04_dataCasting_wrapperClasses/C02_AsciiTable.java
@@ -0,0 +1,23 @@
+package day04_dataCasting_wrapperClasses;
+
+import java.util.Scanner;
+
+public class C02_AsciiTable {
+
+    public static void main(String[] args) {
+
+        // Soru 2 - Kullanicidan bir harf alin ve alfabede o harften sonraki 3 harfi yazin
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Lütfen bir harf giriniz");
+
+        char girilenHarf = scan.next().charAt(0);
+
+        System.out.println("Girilen Harf : " + girilenHarf +
+                            "\nGirilen harften sonraki 3 harf : "
+                            + (char)(girilenHarf+1) +","
+                            + (char)(girilenHarf+2) + ","
+                            + (char)(girilenHarf+3);
+                           }
+}
