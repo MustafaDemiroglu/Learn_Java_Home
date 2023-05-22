6e81e57fc3d4865ed7bfda8ca6cf9c9f05104a1c
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-05-22 19:51:03 +0200

    wrapper

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day04_dataCasting_wrapperClasses/C03_DataCasting.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day04_dataCasting_wrapperClasses/C03_DataCasting.java
@@ -21,7 +21,7 @@ public class C03_DataCasting {
 
         System.out.println(a/d);   // 20/6 = 3,333333
 
-        // a/b isleinin sonucunu nasil ondalkli olarak yazdiracagiz
+        // a/b isleminin sonucunu nasil ondalkli olarak yazdiracagiz
 
         System.out.println((double)(a/b));
         // islem önceligi parantez icinde (a/b) dedir
@@ -33,10 +33,19 @@ public class C03_DataCasting {
         // isleme girenlerden birisi double olmalidir
 
 
-        System.out.println((double) a/b);    // 3.3333333333333335
+        System.out.println((double) a/b);      // 3.3333333333333335
 
         System.out.println(a/(double)b);      //  3.3333333333333335
 
+        int e = 1467 ;
+        System.out.println(e=e/10);           // 146
+        System.out.println(e);                // 146
+        e = e/10;
+        System.out.println(e);                // 14
+        e = e/10;
+        System.out.println(e);                // 1
+        e = e/10;
+        System.out.println(e);                // 0
 
 
     }
