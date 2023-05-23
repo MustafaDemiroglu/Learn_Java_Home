ff848d0ff0d2790603f4fec5d03e35e7a35663b5
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-05-23 21:38:01 +0200

    increment

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day05_incrementDecrement_Concatenation/C01_IncrementDecrement.java
@@ -0,0 +1,24 @@
+package day05_incrementDecrement_Concatenation;
+
+public class C01_IncrementDecrement {
+
+    public static void main(String[] args) {
+
+        int a = 20 ;
+
+        // a degeroni 3 artttirin ve yeni degerini olusturacagniz b variablina atayin
+
+        // a = a+3;
+
+        // a = +3;
+        // System.out.println(a); // 3
+
+        a += 3;
+        System.out.println(a);  // 23
+        int b = a ;
+        System.out.println(b);  //23
+
+        
+
+    }
+}
