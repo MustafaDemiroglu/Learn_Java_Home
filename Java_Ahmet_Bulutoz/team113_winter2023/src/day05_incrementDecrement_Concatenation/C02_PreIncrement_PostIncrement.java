b836902a20d77d7b3e92068ffb3ff7a93979ca8d
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-05-27 10:35:59 +0200

    Update C02_PreIncrement_PostIncrement.java

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day05_incrementDecrement_Concatenation/C02_PreIncrement_PostIncrement.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day05_incrementDecrement_Concatenation/C02_PreIncrement_PostIncrement.java
@@ -7,17 +7,26 @@ public class C02_PreIncrement_PostIncrement {
         // a nin degerini 1 arttirin ve yeni degerini olusturacagniz b variable ina atayin
 
         int a = 20 ;
+        int b= ++a;     // önce arttirir sonrab atama yapar
+        System.out.println("a: " + a + " b: " + b);
 
+        /*
         a++;
         int b=a;
         System.out.println("a: " + a + " b: " + b);
+         */
 
         // a nin degerini b ye atayin sonra a nin dgerini 1 arttirin
 
         a = 20 ;
+        b=a++;  önce atama yapar sonra arttirir
+
+        /*
         b=a;
         a++;
         System.out.println("a: " + a + " b: " + b);
+         */
+
 
 
     }
