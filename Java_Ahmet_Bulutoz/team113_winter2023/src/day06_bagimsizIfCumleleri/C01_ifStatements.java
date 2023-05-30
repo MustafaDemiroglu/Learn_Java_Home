7cc5d9e3342ff3fd51a509de654771c941f56f3f
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-05-30 22:02:23 +0200

    Create C01_ifStatements.java

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day06_bagimsizIfCumleleri/C01_ifStatements.java
@@ -0,0 +1,24 @@
+package day06_bagimsizIfCumleleri;
+
+public class C01_ifStatements {
+
+    public static void main(String[] args) {
+        
+
+
+        int a = 20 ;
+        int b = 30 ;
+
+        if (a > b) {
+            System.out.println("a b'den büyüktür");
+        }
+
+        if (a%2 == 0) {
+            System.out.println("a cift sayidir");
+        }
+
+        if (b > 100) {
+            System.out.println("b 100'den büyüktür");
+        }
+    }
+}
