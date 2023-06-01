68200fe3bf74656b090746b1108b532fdbde21e2
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-06-01 19:45:55 +0200

    Create C03_ifStatements.java

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day06_bagimsizIfCumleleri/C03_ifStatements.java
@@ -0,0 +1,10 @@
+package day06_bagimsizIfCumleleri;
+
+public class C03_ifStatements {
+
+    public static void main(String[] args) {
+
+        // Soru 5 - kullanicidan bir ücgenin 3 kenar uzunlugunu isteyiniz
+        //          eger bu ücgen eskenar ücgen ise ekrana "eskenar ücgen" yazdirin
+    }
+}
