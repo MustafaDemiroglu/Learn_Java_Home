c3516fcf7bf34a45e982a06266652f53165bd776
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-06-01 21:56:27 +0200

    C07_ifElseStatements

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day06_bagimsizIfCumleleri/C07_ifElseStatements.java
@@ -0,0 +1,24 @@
+package day06_bagimsizIfCumleleri;
+
+import java.util.Scanner;
+
+public class C07_ifElseStatements {
+
+    public static void main(String[] args) {
+
+        // Soru 4 - Kullanicidan bir karakter girmesini isteyin ,
+        //          girilen karakterin büyük harf olup olmadigini yazdirin
+
+        Scanner scan = new Scanner(System.in) ;
+        System.out.println(" Lütfen bir karakter giriniz :");
+        char krk =  scan.next().charAt(0);
+
+        // r ===> A ile Z arasinda mi?
+
+        if ('A'<=krk && krk<='Z') {
+            System.out.println("Girmis oldugunuz karakter " + krk + " büyük bir harftir");
+        }  else {
+            System.out.println("Girmis oldugunuz karakter " + krk + "  büyük bir harf degildir");
+        }
+    }
+}
