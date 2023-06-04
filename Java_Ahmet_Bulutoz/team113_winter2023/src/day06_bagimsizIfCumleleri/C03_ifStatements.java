e788709a15b23e7bfe7b5d6f7116b8140b2e9a0c
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-06-04 22:44:08 +0200

    Update C03_ifStatements.java

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day06_bagimsizIfCumleleri/C03_ifStatements.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day06_bagimsizIfCumleleri/C03_ifStatements.java
@@ -6,7 +6,7 @@ public class C03_ifStatements {
 
     public static void main(String[] args) {
 
-        // Soru 5 - kullanicidan bir ücgenin 3 kenar uzunlugunu isteyiniz
+        // Soru 5 - Kullanicidan bir ücgenin 3 kenar uzunlugunu isteyiniz
         //          eger bu ücgen eskenar ücgen ise ekrana "eskenar ücgen" yazdirin
 
         Scanner scan = new Scanner (System.in);
