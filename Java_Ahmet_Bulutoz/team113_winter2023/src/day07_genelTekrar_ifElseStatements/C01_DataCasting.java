99bb24780982cf52da41894c72963e84c3265b98
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-06-07 23:06:26 +0200

    Update C01_DataCasting.java

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day07_genelTekrar_ifElseStatements/C01_DataCasting.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day07_genelTekrar_ifElseStatements/C01_DataCasting.java
@@ -2,5 +2,14 @@ package day07_genelTekrar_ifElseStatements;
 
 public class C01_DataCasting {
 
-    // 
+    // Java'da bazi Data türleri asla baska data türüne cevrilemez
+
+    /*
+     * String str1 = false ;
+     * String str2 = 20 ;
+     * boolean bl1 = 'a';
+     * int sayi1 = "Merhaba";
+     */
+
+    
 }
