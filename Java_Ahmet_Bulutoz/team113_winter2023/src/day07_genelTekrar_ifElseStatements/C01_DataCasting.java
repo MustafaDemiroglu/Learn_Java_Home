49ee0b947918dc6154fba54f182e83b8c74f2dbf
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-06-12 21:43:50 +0200

    update data casting

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day07_genelTekrar_ifElseStatements/C01_DataCasting.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day07_genelTekrar_ifElseStatements/C01_DataCasting.java
@@ -16,6 +16,35 @@ public class C01_DataCasting {
     // özellikle sayisal data türündeki degerler,
     // baska sayisal data türündeki variable a atanabilir
 
+        double db1 = 23 ;
+        short sh1 = 12 ;
+
+        int sayi2= sh1;
+
+        //daha dar data türündeki degeri daha genis data türündeki variable atamaya
+        // autowidening denir ve java bunu otomatik yapar
+
+        sh1 = (short)sayi2;
+        sayi2 = (int)db1;
+
+        // daha genis dazta türündeki degeri, daha dar data türündeki variable a atamaya
+        // explicit narrowing denir ve java bunu otomatik yapmaz
+        // biztden onay ister
+
+        sayi2 = 'k';
+
+        if (sayi2>'d'){
+
+        }
+
+        // char data türündeki degerler sayisal islemlerde kullanilabilirler
+        // bir char matematiksel islemde kullanilirsa ASCII table'daki degeri le isleme girer
+
+        System.out.println('k'+1);      // 108
+        // eger bu islemin char olarak degerini yazdirmak istersek
+        // sayisal degerler char a cast edilebilir
+
+        System.out.println((char)('k'+1));
 
 }
 }
