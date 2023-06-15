12b6215d7729caf4b1dab124403201acaf8d757d
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-06-15 22:39:56 +0200

    Update C03_Concatenation.java

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day05_incrementDecrement_Concatenation/C03_Concatenation.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day05_incrementDecrement_Concatenation/C03_Concatenation.java
@@ -4,7 +4,7 @@ public class C03_Concatenation {
 
     public static void main(String[] args) {
 
-        // Bir String baska bir String veya baska bir data türünden bir bvariable ile
+        // Bir String baska bir String veya baska bir data türünden bir variable ile
         // + isareti ile birlestirilebilir
 
         String a = "Java";
@@ -19,7 +19,7 @@ public class C03_Concatenation {
         // Eger String ile baska data türündeki bir deger toplanirsa Java sonucu String yapar
         System.out.println(c+d+a);      // 30Java
 
-        // Peki Java30 yazmak sitersek
+        // Peki Java30 yazmak istersek
         System.out.println(a+(c+d));    // Java30
 
         System.out.println(a+c*d);      // Java200
