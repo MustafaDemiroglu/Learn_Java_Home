dd132f11f7078e5bf0fa3b17e4cbf9df3ab983a3
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-06-16 23:48:17 +0200

    Update C01_HelloWorld.java

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day01_javayaGiris/C01_HelloWorld.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day01_javayaGiris/C01_HelloWorld.java
@@ -3,7 +3,7 @@ package day01_javayaGiris;
 public class C01_HelloWorld {
 
     public static void main(String[] args) {
-       // sout yazip entera basarsaniz konsolda yazdirma kodu oln System.out.print.ln() cikar
+       // sout yazip entera basarsaniz konsolda yazdirma kodu olan System.out.print.ln() cikar
        // java aciklama satirlarini görmezden gelir
        // IntelliJ de bu aciklama yazilarini gri renkli yapar
         System.out.println("Hello World");
