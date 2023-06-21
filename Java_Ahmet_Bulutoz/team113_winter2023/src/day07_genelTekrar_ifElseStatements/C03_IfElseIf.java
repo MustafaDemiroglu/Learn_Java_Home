2c7c29eca85f27fb6f959b6cd7179d968af64aa2
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-06-21 21:34:07 +0200

    Create C03_IfElseIf.java

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day07_genelTekrar_ifElseStatements/C03_IfElseIf.java
@@ -0,0 +1,13 @@
+package day07_genelTekrar_ifElseStatements;
+
+public class C03_IfElseIf {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan pozitif bir tamsayi alin
+        // sayi 3 ile bölünebiliyorsa "3ün kati"
+        // sayi 5 ile bölünebiliyorsa "5"in kati
+        // sayi hem 3 hem de 5 ile bölünebiliyorsa "3" ve "5" in kati
+        //
+    }
+}
