6f6c0601b9e170a62e1ac4dd367491192075e28b
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-06-23 23:33:31 +0200

    Update C01_ArtikYil.java

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day08_NestedIfelse_ternary/C01_ArtikYil.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day08_NestedIfelse_ternary/C01_ArtikYil.java
@@ -10,7 +10,7 @@ public class C01_ArtikYil {
      * Kural 1: 4 ile bolunemeyen yillar artik yil degildir.
      * Kural 2: 4 ile bolunup 100 ile bolunemeyen yillar artik yildir.
      * Kural 3: 4’un kati olmasina ragmen 100 ile bolunebilen yillardan
-     * sadece 400’un kati olan yillar artik yildir
+     * sadece 400’un kati olan yillar artik yildir.
      */
 
     public static void main(String[] args) {
