264a689c591b7f836229058c238743f76fc9f169
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-06-23 23:33:08 +0200

    updte

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day08_NestedIfelse_ternary/C01_ArtikYil.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day08_NestedIfelse_ternary/C01_ArtikYil.java
@@ -7,7 +7,7 @@ public class C01_ArtikYil {
     /*
      * Interview Question
      * Kullanicidan artik yil olup olmadigini kontrol etmek icin bir yil girmesini isteyin.
-     * Kural 1: 4 ile bolunemeyen yillar artik yil degildir
+     * Kural 1: 4 ile bolunemeyen yillar artik yil degildir.
      * Kural 2: 4 ile bolunup 100 ile bolunemeyen yillar artik yildir
      * Kural 3: 4’un kati olmasina ragmen 100 ile bolunebilen yillardan
      * sadece 400’un kati olan yillar artik yildir
