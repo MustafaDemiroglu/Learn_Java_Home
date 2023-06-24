5a9e15b93720a53696634b8865f1b3a19e83a06b
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-06-24 23:50:29 +0200

    Update C02_NestedIfElse.java

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day08_NestedIfelse_ternary/C02_NestedIfElse.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day08_NestedIfelse_ternary/C02_NestedIfElse.java
@@ -1,9 +1,12 @@
 package day08_NestedIfelse_ternary;
 
+import java.util.Scanner;
+
 public class C02_NestedIfElse {
     public static void main(String[] args) {
 
 
+        Scanner scan = new Scanner(System.in);
         
     }
 }
