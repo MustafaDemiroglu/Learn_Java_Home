4f57ea38eaff593787fd808cf819940c29d0faa8
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-06-25 22:20:15 +0200

    nested if else

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day08_NestedIfelse_ternary/C03_NestedIfElse.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day08_NestedIfelse_ternary/C03_NestedIfElse.java
@@ -1,8 +1,48 @@
 package day08_NestedIfelse_ternary;
 
+import java.util.Scanner;
+
 public class C03_NestedIfElse {
 
     public static void main(String[] args) {
-        
+        /*
+         * Kullanicidan cinsiyetini ve asini alin
+         * Kadin 60 yas ve üzewri erkek 65 yas ve üzeri mrkli olabilirler
+         * cinsiyet ve yasini dikkate alarak emekli olabilirisn
+         * veya emekli olabilmek icin su kadar yil daha caliman gerek yazdirin
+         * hatali giris yapilirsa kullaniciyi uyarin
+         */
+
+        // bu defa ana degisken yas olsun
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lütfen yasinizi giriniz");
+        double yas = scan.nextDouble();
+
+        System.out.println("lütfen cinsiyetini erkek ise E Kadin ise K olarak giriniz ");
+        char cinsiyet = scan.next().charAt(0);
+
+        if (yas <16 || yas>80) {
+            System.out.println("Gecersiz yas");
+        } else if (yas <60) {
+            if (cinsiyet == 'k' || cinsiyet=='K') {
+                System.out.println("Emekli olmak icin " + (60-yas) + " yil daha calismalisin");
+            } else if (cinsiyet == 'e' || cinsiyet=='E') {
+                System.out.println("Emekli olmak icin " + (65-yas) + " yil daha calismalisin");
+            } else {
+                System.out.println("Gecersiz cinsiyet girisi");
+            }
+        } else if (yas <65) {
+            if (cinsiyet == 'k' || cinsiyet=='K') {
+                System.out.println("Emekli olabilirsin");
+            } else if (cinsiyet == 'e' || cinsiyet=='E') {
+                System.out.println("Emekli olmak icin " + (65-yas) + " yil daha calismalisin");
+            } else {
+                System.out.println("Gecersiz cinsiyet girisi");
+            }
+        } else {
+            System.out.println("Emekli olabilirsin");
+        }
+
     }
 }
