aaecb539cedd33f811d9a963ce9e0f02d346664d
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-06-25 21:41:16 +0200

    Update C02_NestedIfElse.java

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day08_NestedIfelse_ternary/C02_NestedIfElse.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day08_NestedIfelse_ternary/C02_NestedIfElse.java
@@ -5,6 +5,16 @@ import java.util.Scanner;
 public class C02_NestedIfElse {
     public static void main(String[] args) {
 
+        /*
+         * Kullanicidan cinsiyetini ve yasini alin,
+         * Kadin 60 yas ve üzeri , erkek 65 yas ve üzeri emekli olabilir
+         * Cinsiyet ve yasini dikkate alarak "Emekli olabilirsin"
+         * veya "Emekli olmak icin  ..... yil daha calisman gerekir" yazdirin
+         * hatali giri yapilirsa kullaniciyi uyarin
+         */
+
+        // birden fazla degiskene bagli if else statementleri yazarken
+
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Lütfen bir sayi giriniz : ");
