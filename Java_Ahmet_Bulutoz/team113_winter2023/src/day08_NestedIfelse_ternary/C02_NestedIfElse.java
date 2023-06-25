665a23e9be5f65c5cddfd5b5cc561b265447d854
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-06-25 21:58:35 +0200

    nested if else

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day08_NestedIfelse_ternary/C02_NestedIfElse.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day08_NestedIfelse_ternary/C02_NestedIfElse.java
@@ -14,10 +14,38 @@ public class C02_NestedIfElse {
          */
 
         // birden fazla degiskene bagli if else statementleri yazarken
+        // önce degiskenlerden birine göre ana yapiyi olusturalim
+        // bu soruda cinsiyeti ana degisken yapalim
 
+        // iinci degiskenin her bölümünde ayrui ayri degerlendirelim
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Lütfen bir sayi giriniz : ");
-        int sayi = scan.nextInt();
+
+        System.out.println("Lütfen yasinizi giriniz : ");
+        double yas = scan.nextDouble();
+
+        System.out.println("lütfen cinsiytinizi giriniz....."
+                            + "\nKadin icin : K , erkek icin : E giriniz ");
+        char cinsiyet = scan.next().charAt(0);
+
+        if (cinsiyet == 'k'|| cinsiyet =='K') {
+            if (yas <16 || yas>80) {
+                System.out.println("Gecersiz yas girisi, lütfen tekrar dendeyin");
+            } else if (yas<60) {
+                System.out.println("Emekli olmak icin daha " + (60-yas) + " yil calismalisin");
+            } else {
+                System.out.println("Emekli olabilirsin");
+            }
+        } else if (cinsiyet == 'e'|| cinsiyet =='E') {
+            if (yas <16 || yas>80) {
+                System.out.println("Gecersiz yas girisi, lütfen tekrar dendeyin");
+            } else if (yas<65) {
+                System.out.println("Emekli olmak icin daha " + (65-yas) + " yil calismalisin");
+            } else {
+                System.out.println("Emekli olabilirsin");
+            }
+        } else {
+            System.out.println("Gecersiz cinsiyet girisi, lütfen yeniden deneyiniz");
+        }
     }
 }
