53119a4ca6d237471513b6812cb6f97959e4666c
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-06-26 21:22:20 +0200

    Create C05_Ternary.java

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day08_NestedIfelse_ternary/C05_Ternary.java
@@ -0,0 +1,27 @@
+package day08_NestedIfelse_ternary;
+
+import java.util.Scanner;
+
+public class C05_Ternary {
+
+    public static void main(String[] args) {
+
+        // kullanicidan bir tamsayi alip
+        // mutlak degerine cevirin
+        // ekrana yazdirin
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lütfen bir tam sayi giriniz : ");
+        int sayi = scan.nextInt();
+
+        
+
+        if (sayi >= 0) {
+            System.out.println(sayi);
+        } else {
+            sayi=sayi*(-1);
+            System.out.println(sayi);
+        }
+
+    }
+}
