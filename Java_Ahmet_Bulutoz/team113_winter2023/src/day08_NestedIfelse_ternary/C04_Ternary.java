12703b62fdceb1afa8f10ec7fc5396534d0cdac6
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-06-26 21:18:42 +0200

    ternary

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day08_NestedIfelse_ternary/C04_Ternary.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day08_NestedIfelse_ternary/C04_Ternary.java
@@ -1,5 +1,7 @@
 package day08_NestedIfelse_ternary;
 
+import java.util.Scanner;
+
 public class C04_Ternary {
 
     public static void main(String[] args) {
@@ -8,6 +10,19 @@ public class C04_Ternary {
         // sayi cift ise sayi cift
         // degilse tek sayi yazdirin
 
-        
+        Scanner scan = new Scanner(System.in) ;
+        System.out.println("Lütfen pozitif bir tam sayi giriniz");
+        int sayi = scan.nextInt() ;
+
+        System.out.println("-------- if else --------");
+
+        if (sayi %2 ==0) {
+            System.out.println("Cift sayi");
+        } else {
+            System.out.println("Tek sayi");
+        }
+
+        System.out.println("-------- Ternary -----------");
+        System.out.println(sayi%2==0? "Cift sayi" : "Tek sayi") ;
     }
 }
