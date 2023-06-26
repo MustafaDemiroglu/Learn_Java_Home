dd05896956922e42640e26018369c99b47062a9c
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-06-26 21:14:48 +0200

    Create C04_Ternary.java

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day08_NestedIfelse_ternary/C04_Ternary.java
@@ -0,0 +1,13 @@
+package day08_NestedIfelse_ternary;
+
+public class C04_Ternary {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan bir sayi alin
+        // sayi cift ise sayi cift
+        // degilse tek sayi yazdirin
+
+        
+    }
+}
