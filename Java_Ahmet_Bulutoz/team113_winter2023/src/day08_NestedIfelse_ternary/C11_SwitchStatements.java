383e9a6e977aa8e2224c8815b42801bfb460a25f
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-07-01 21:35:32 +0200

    Create C11_SwitchStatements.java

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day08_NestedIfelse_ternary/C11_SwitchStatements.java
@@ -0,0 +1,10 @@
+package day08_NestedIfelse_ternary;
+
+public class C11_SwitchStatements {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan bir harf isteyin
+        // 
+    }
+}
