436da4e89d20400c460844bd84866706178e4eb8
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-07-01 21:36:16 +0200

    Update C11_SwitchStatements.java

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day08_NestedIfelse_ternary/C11_SwitchStatements.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day08_NestedIfelse_ternary/C11_SwitchStatements.java
@@ -5,6 +5,7 @@ public class C11_SwitchStatements {
     public static void main(String[] args) {
 
         // Kullanicidan bir harf isteyin
-        // 
+        // O harf ile baslayan gün ismi var ise yazdirin
+        // yoksa "gecersiz harf yazdirin
     }
 }
