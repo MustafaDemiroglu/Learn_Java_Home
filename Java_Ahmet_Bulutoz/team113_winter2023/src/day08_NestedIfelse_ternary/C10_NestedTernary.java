3e2e4c922757c2a80adf410e587630c84225df52
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-07-01 20:57:01 +0200

    Create C10_NestedTernary.java

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day08_NestedIfelse_ternary/C10_NestedTernary.java
@@ -0,0 +1,17 @@
+package day08_NestedIfelse_ternary;
+
+public class C10_NestedTernary {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan bir tamsayi alin
+        // Sayi pozitifse, cift sayi veya cift sayi degil seceneklerinden uygun olani yazdirin
+        // Sayi pozitif degilse, 3 basamakli veya 3 basamakli degil seceneklerinden
+        // uygun olani yazdirin
+
+
+        int sayi = 20 ;
+
+        System.out.println(sayi>0 ? : );
+    }
+}
