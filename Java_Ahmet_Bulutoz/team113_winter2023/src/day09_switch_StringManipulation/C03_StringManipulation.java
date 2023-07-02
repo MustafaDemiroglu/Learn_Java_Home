f900772ebe889aac8c6d732a0579c97b734e917a
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-07-02 20:51:18 +0200

    Manipulation

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day09_switch_StringManipulation/C03_StringManipulation.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day09_switch_StringManipulation/C03_StringManipulation.java
@@ -1,11 +1,29 @@
 package day09_switch_StringManipulation;
 
+import java.util.Locale;
+
 public class C03_StringManipulation {
 
     public static void main(String[] args) {
 
         String str = "Java Candir";
 
-        // 
+        // str buyuk harf yazdirin
+
+        System.out.println(str.toUpperCase());  // JAVA CANDIR
+
+        System.out.println(str);        // Java Candir
+
+        // String manipulaton yaptiktan sonra, ail variable atama yapilmazsa
+        // asil variable degismez
+
+        str = str.toUpperCase();
+        System.out.println(str);        // JAVA CANDIR
+        System.out.println(str.toLowerCase());      // java candir
+
+        // str türkce karakterleri gözönünde bulundurarark kücük harfe cevirin
+
+        str = str.toLowerCase(Locale.forLanguageTag("TR"));
+        System.out.println(str);    // java candir
     }
 }
