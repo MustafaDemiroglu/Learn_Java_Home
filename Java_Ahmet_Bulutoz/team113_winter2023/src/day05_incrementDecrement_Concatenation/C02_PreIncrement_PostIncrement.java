0fbb314eec04c2d7e4f95ae9e86890ee3e3cdacc
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-07-05 22:23:19 +0200

    Update C02_PreIncrement_PostIncrement.java

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day05_incrementDecrement_Concatenation/C02_PreIncrement_PostIncrement.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day05_incrementDecrement_Concatenation/C02_PreIncrement_PostIncrement.java
@@ -5,10 +5,10 @@ public class C02_PreIncrement_PostIncrement {
     public static void main(String[] args) {
 
         /*
-         * Eger sayinin degrei SADECE 1 artacak veya azalacaksa
+         * Eger sayinin degeri SADECE 1 artacak veya azalacaksa
          * arttirip sonra yazdirma / atama
-         * yazdirip sonra arttirma/atama
-         * islemlerinin tek satirda yapabiliriz
+         * yazdirip sonra arttirma / atama
+         * islemlerini tek satirda yapabiliriz
          *
 
          */
@@ -18,7 +18,7 @@ public class C02_PreIncrement_PostIncrement {
         // a nin degerini 1 arttirin ve yeni degerini olusturacagniz b variable ina atayin
 
         int a = 20 ;
-        int b= ++a;     // önce arttirir sonrab atama yapar
+        int b= ++a;     // önce arttirir sonra b ye atama yapar
         System.out.println("a: " + a + " b: " + b);
 
         /*
@@ -27,7 +27,7 @@ public class C02_PreIncrement_PostIncrement {
         System.out.println("a: " + a + " b: " + b);
          */
 
-        // a nin degerini b ye atayin sonra a nin dgerini 1 arttirin
+        // a nin degerini b ye atayin sonra a nin degerini 1 arttirin
 
         a = 20 ;
         b=a++;      // önce atama yapar sonra arttirir
@@ -43,9 +43,9 @@ public class C02_PreIncrement_PostIncrement {
         System.out.println("a: " + a + " b: " + b);
 
 
-        // Bu islemleri istrsek tek satirda da yapabiiriz
+        // Bu islemleri istersek tek satirda da yapabiiriz
 
-        // a nin degreinin yazdirin ve sponra ayi 1 artirin
+        // a nin degerini yazdirin ve sonra ayi 1 artirin
 
         a=20 ;
         System.out.println("a : " + a);      // a:20
