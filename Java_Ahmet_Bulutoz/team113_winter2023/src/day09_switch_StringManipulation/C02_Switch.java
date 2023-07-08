6047822da37f97a49fe4598826fd4ad4ba5ff37b
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-07-08 23:51:00 +0200

    Update C02_Switch.java

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day09_switch_StringManipulation/C02_Switch.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day09_switch_StringManipulation/C02_Switch.java
@@ -7,32 +7,16 @@ public class C02_Switch {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.println( "Lütfen hangi mevsimde oldugunu ögrenmek istediginiz ayin numarasini giriniz ");
+        System.out.println( "Lütfen hangi mevsimde oldugunu ögrenmek istediginiz ayin numarasini tam sayi olarak giriniz ");
         int ayNo = scan.nextInt();
         scan.close();
 
         switch (ayNo) {
-            case 12:
-            case 1:
-            case 2:
-                System.out.println("Kis");
-                break;
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("Ilkbahar");
-                break;
-            case 6:
-            case 7:
-            case 8:
-                System.out.println("Yaz");
-                break;
-            case 9:
-            case 10:
-            case 11:
-                System.out.println("Sonbahar");
-                break;
-
+            case 12, 1, 2 -> System.out.println("Kis");
+            case 3, 4, 5 -> System.out.println("Ilkbahar");
+            case 6, 7, 8 -> System.out.println("Yaz");
+            case 9, 10, 11 -> System.out.println("Sonbahar");
+            default -> throw new IllegalStateException("Unexpected value: " + ayNo);
         }
     }
 }
