2a213711a3bf3f4779ba16423bbedf4420719912
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-07-10 21:49:26 +0200

    substring

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day10_stringManipulation/C01_substring.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day10_stringManipulation/C01_substring.java
@@ -6,6 +6,9 @@ public class C01_substring {
 
         String str = "Java ne kadar güzel";
 
+        // substring iki farkli sekilde kullanilabilir
+        // 1- tek parca parametre olursa , o index den baslar sonuna kadar yazdirir
+
         System.out.println(str.substring(8));       // kadar güzel
 
         // System.out.println(str.substring(0));       // Java ne kadar güzel
@@ -16,11 +19,25 @@ public class C01_substring {
 
         System.out.println(str.substring(str.length()));        // hiclik
 
-        System.out.println(str.substring(str.charAt(str.length())));        // error
+        // System.out.println(str.substring(str.charAt(str.length())));
+        // error
         // StringIndexOutOfBoundsException:
         // Index 19 out of bounds for length 19
         // str in uzunlugu 19 son harfin index i 18
         // 19. index te karakter yok
+
+
+        // 2 - iki parametre girilirse bslangic index in den (dahil) bsaslar
+        // bitisindexine (haric) kadar olan bölümü yazdirir.
+
+        System.out.println(str.substring(5,7));         // ne
+
+        System.out.println(str.substring(3,4));         // a
+        System.out.println(str.charAt(3));              // a
+
+
+        System.out.println(str.substring(0,1));         // J
+        System.out.println(str.charAt(0));              // J
 
     }
 }
