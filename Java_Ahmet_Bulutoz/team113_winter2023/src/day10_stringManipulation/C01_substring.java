448c9fea5c4f8bf8583dbb85a60a5695cf2ff7f2
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-07-10 21:36:48 +0200

    Create C01_substring.java

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day10_stringManipulation/C01_substring.java
@@ -0,0 +1,13 @@
+package day10_stringManipulation;
+
+public class C01_substring {
+
+    public static void main(String[] args) {
+
+        String str = "Java ne kadar güzel";
+
+        System.out.println(str.substring(8));       // kadar güzel
+
+        System.out.println(str.substring(0));       // Java ne kadar güzel
+    }
+}
