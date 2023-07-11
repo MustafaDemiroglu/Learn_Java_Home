9aa45b0e9e64fde4b30abf282399cde5467935dc
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-07-11 22:49:11 +0200

    Update C04_equals.java

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day09_switch_StringManipulation/C04_equals.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day09_switch_StringManipulation/C04_equals.java
@@ -28,7 +28,7 @@ public class C04_equals {
         System.out.println(str1 == str8);       // Java == Java ama false
 
         /*
-         * == ile Stringleri karsilastirirsak hem metin deggerine hem de referanslarina bakar
+         * == ile Stringleri karsilastirirsak hem metin degerine hem de referanslarina bakar
          * equals ile String leri karsilastirirsak sadece metin degerlerine bakar
          *
          * == ile Stringleri karsilastirdigimizda bekledigimizden farkli sonuclar olabilir
