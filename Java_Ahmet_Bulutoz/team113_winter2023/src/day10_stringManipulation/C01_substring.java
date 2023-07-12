368fec0ee3945547e2e019461a736862ffc8d759
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-07-12 22:32:04 +0200

    Update C01_substring.java

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day10_stringManipulation/C01_substring.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day10_stringManipulation/C01_substring.java
@@ -28,7 +28,7 @@ public class C01_substring {
 
 
         // 2 - iki parametre girilirse baslangic index in den (dahil) baslar
-        // bitis indexine (haric) kadar olan bölümü yazdirir.
+        // bitis index ine (haric) kadar olan bölümü yazdirir.
 
         System.out.println(str.substring(5,7));         // ne
 
