af927888c0ce82d2601f02a2b749e4eae2a185c3
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-07-13 21:52:20 +0200

    Create C02_contains.java

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day10_stringManipulation/C02_contains.java
@@ -0,0 +1,11 @@
+package day10_stringManipulation;
+
+public class C02_contains {
+
+    public static void main(String[] args) {
+
+        String str = "Java ogren, isi kap";
+
+        System.out.println(str.contains("Java"));       // true
+    }
+}
