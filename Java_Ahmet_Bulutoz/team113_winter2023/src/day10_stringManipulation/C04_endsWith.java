ae295c0538871393ce39071b915c600d6f34ffd3
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-07-20 21:48:17 +0200

    Update C04_endsWith.java

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day10_stringManipulation/C04_endsWith.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day10_stringManipulation/C04_endsWith.java
@@ -7,7 +7,7 @@ public class C04_endsWith {
     public static void main(String[] args) {
 
         /* SORU: Kullanicidan bir e-mail adresi alin
-         * - girilen mail adresi @ icermiyorsa "gecersiz e-mail adresi girdiniz"
+         * - girilen mail adresi @ icermiyorsa "gecersiz bir e-mail adresi girdiniz"
          * - girilen mail adresi @gmail icermiyorsa "gecersiz e-mail adresi girdiniz "
          * - girilen mail adresi @gmail.com ile bitmiyorsa "mailde yazim hatasi var" yazdirin.
         */
