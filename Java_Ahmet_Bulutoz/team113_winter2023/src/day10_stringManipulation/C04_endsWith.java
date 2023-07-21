33739dad2f26c80a658374488771d2413fecfd81
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-07-21 23:51:10 +0200

    Update C04_endsWith.java

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day10_stringManipulation/C04_endsWith.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day10_stringManipulation/C04_endsWith.java
@@ -18,7 +18,7 @@ public class C04_endsWith {
         scan.close();
 
         if (!email.contains("@"))  {
-            System.out.println("gecersiz email aresi");
+            System.out.println("gecersiz email adresi");
         } else if (!email.contains("@gmail.com")) {
             System.out.println("Mail adresi gmail olmali");
         } else if (!email.endsWith("gmail.com")) {
