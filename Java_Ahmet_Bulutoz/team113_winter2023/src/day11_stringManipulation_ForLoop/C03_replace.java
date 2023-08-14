976ca95fe099c482ccf666c33ffc341b6c62005b
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-08-14 19:19:38 +0200

    replace

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day11_stringManipulation_ForLoop/C03_replace.java
@@ -0,0 +1,13 @@
+package day11_stringManipulation_ForLoop;
+
+public class C03_replace {
+
+    public static void main(String[] args) {
+
+
+        String str = "Java Candir";
+
+        System.out.println(str.replace('J','H'));       // Hava Candir
+        System.out.println(str.replace("a",""));        // Jv Cndir
+    }
+}
