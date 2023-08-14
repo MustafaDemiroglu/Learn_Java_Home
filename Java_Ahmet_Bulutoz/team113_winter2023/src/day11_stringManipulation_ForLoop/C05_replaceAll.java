583a9a4bd625cbeb6b4ebe0601b672ee95d54d2b
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-08-14 22:12:17 +0200

    replace

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day11_stringManipulation_ForLoop/C05_replaceAll.java
@@ -0,0 +1,23 @@
+package day11_stringManipulation_ForLoop;
+
+public class C05_replaceAll {
+
+    public static void main(String[] args) {
+
+        String str = "          J&ava54 C+an01dir,.....:             ";
+        System.out.println(str);
+
+        str = str.trim();
+        System.out.println("trimden sonra str : " + str);
+
+        // str'i Java Candir haline getirin
+
+        str = str.replaceAll("\\d","");
+        System.out.println("str yeni hali : " + str);
+
+        str = str.replaceAll(" ","5");
+        str = str.replaceAll("\\W", "");
+        str = str.replaceAll("5", " ");
+        System.out.println("str nin son hali : " + str);
+    }
+}
