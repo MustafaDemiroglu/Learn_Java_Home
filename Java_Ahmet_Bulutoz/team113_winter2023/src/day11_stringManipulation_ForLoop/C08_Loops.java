40ccbbac264f1e85da3a72a00957b4fcfecdc944
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-08-28 20:28:13 +0200

    ForLoop

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day11_stringManipulation_ForLoop/C08_Loops.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day11_stringManipulation_ForLoop/C08_Loops.java
@@ -7,7 +7,11 @@ public class C08_Loops {
         // 10 kere Java Candir" yazdirin
 
         for (int i = 1; i <=10 ; i++) {
-            System.out.println("Java Candir");
+            System.out.println(i + " - Java Candir");
+        }
+
+        for (int i = 1; i <= 41; i++) {
+            System.out.println( i + " kere Masallah");
         }
     }
 }
---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day11_stringManipulation_ForLoop/C09_ForLoop.java
@@ -0,0 +1,40 @@
+package day11_stringManipulation_ForLoop;
+
+public class C09_ForLoop {
+
+    public static void main(String[] args) {
+
+        // 50 den baslayip, 100 e kadar (100 dahil) sayilari yanyana
+        // aralarinda bir bosluk olacak sekilde yazdirin
+        for (int i = 50; i <=100 ; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+
+        // 3 basmakli sayilardan 23 ile bölünebilenleri
+        // yan yana , aralarinda bir bosluk olacak sekilde yazdirin
+        for (int i = 100; i <=1000 ; i++) {
+            if (i%23==0){
+                System.out.print(i+ " ");
+            }
+        }
+        System.out.println("");
+
+        // 20'den baslayip 7 ser artarak 100 e kadar yanyana yazdirin
+        // (100 yazdirmaniz gerekiyorsa yazdirin ama 100u gecmeyin)
+        for (int i = 20; i <=100 ; i+=7) {
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+
+        // 100 den baslayarak 1 e kadar sinirlar dahil
+        // 7 ile bölünebilen sayilari yanyana yazdirin
+        for (int i = 100; i >=1 ; i--) {
+            if (i%7==0) {
+                System.out.print(i + " ");
+            }
+        }
+
+
+    }
+}
