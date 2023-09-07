51e05371984bff815459b189c3f076e9ab8bfd13
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-09-07 20:13:11 +0200

    for Loops

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day12_forLoops/C01_DikkatEdilecekler.java
@@ -0,0 +1,13 @@
+package day12_forLoops;
+
+public class C01_DikkatEdilecekler {
+
+    public static void main(String[] args) {
+
+        for (int i=20;i<20;i++){
+            System.out.print(i + " ");
+
+        }
+    }
+
+}
