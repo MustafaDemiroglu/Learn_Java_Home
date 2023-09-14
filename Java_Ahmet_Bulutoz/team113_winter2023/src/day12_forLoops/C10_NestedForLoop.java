02038e5106915f16475ecbf3fd80cc8a557b4024
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-09-14 21:27:58 +0200

    Nested For Loop

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day12_forLoops/C10_NestedForLoop.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day12_forLoops/C10_NestedForLoop.java
@@ -1,14 +1,12 @@
 package day12_forLoops;
 
-public class C09_NestedForLoop {
+public class C10_NestedForLoop {
     public static void main(String[] args) {
         for (int i = 1; i <= 5 ; i++) {
-            for (int j = 1; j <i ; j++) {
+            for (int j = 6; j >i ; j--) {
                 System.out.print(" * ");
             }
             System.out.println("");
         }
-
-
     }
 }
