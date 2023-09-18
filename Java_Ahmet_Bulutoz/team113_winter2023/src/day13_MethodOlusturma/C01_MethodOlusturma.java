4bec789f6be5c8077a55f27ec0843217aff3341e
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-09-18 21:40:59 +0200

    method create

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day13_MethodOlusturma/C01_MethodOlusturma.java
@@ -0,0 +1,17 @@
+package day13_MethodOlusturma;
+
+public class C01_MethodOlusturma {
+    public static void main(String[] args) {
+        String str = "Java Candir";
+
+        // str i büyük harflere cevirin
+
+        System.out.println("-------------------- 1.yol -----------------");
+
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+
+        System.out.println("------------- 2. yol ----------------");
+
+    }
+}
---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day13_MethodOlusturma/C02_MethodOlusturma.java
@@ -0,0 +1,20 @@
+package day13_MethodOlusturma;
+
+import java.util.Scanner;
+
+public class C02_MethodOlusturma {
+    public static void main(String[] args) {
+        // Kullanicidan iki sayi alip
+        // toplamlarini yazdiran bir method olusturun
+
+    }
+
+    public static void ikiSayiyiTopla(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println(" Toplamak üzere 2 sayi giriniz");
+        double sayi1 = scan.nextDouble();
+        double sayi2 = scan.nextDouble();
+
+        System.out.println(" iki sayinin toplami : " + (sayi1+sayi2));
+    }
+}
