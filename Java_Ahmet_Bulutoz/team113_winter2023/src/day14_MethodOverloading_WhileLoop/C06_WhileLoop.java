d5cf7559faafc022fe40cba953ac04e0ef20bb62
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-09-23 19:12:20 +0200

    While Loop

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day14_MethodOverloading_WhileLoop/C06_WhileLoop.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day14_MethodOverloading_WhileLoop/C06_WhileLoop.java
@@ -5,9 +5,27 @@ import java.util.Scanner;
 public class C06_WhileLoop {
     public static void main(String[] args) {
         // Kullanicidan toplanmak üzere sayilar alin
-        // Toplam 500 ü asinca
+        // Toplam 500 olunca veya 500 ü asinca
         // kac sayi girdigini ve sayilarin toplamini yazdirin
 
         Scanner scan= new Scanner(System.in);
+
+
+        int girilenSayi = 0 ;
+        int toplam = 0 ;
+        int girilenSayiAdedi = 0 ;
+
+        while (toplam<500){     // Toplam 500 den kücük oldugu süre boyunca
+            System.out.println("Su ana kadar " + girilenSayiAdedi + " adet tam sayi girdiniz");
+            System.out.println("Simdiye kadar girmis oldugunuz sayilar toplami : " + toplam);
+            System.out.println("Toplanmak üzere bir tamsayi giriniz : ");
+            girilenSayi = scan.nextInt();
+            toplam+=girilenSayi;
+            girilenSayiAdedi++;
+        }
+
+        System.out.println(girilenSayiAdedi + " defa sayi girisi yaptiniz " +
+                "\nve girdiginiz sayilarin toplami :  " + toplam);
+
     }
 }
---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day14_MethodOverloading_WhileLoop/C07_WhileLoop.java
@@ -0,0 +1,7 @@
+package day14_MethodOverloading_WhileLoop;
+
+public class C07_WhileLoop {
+    public static void main(String[] args) {
+
+    }
+}
