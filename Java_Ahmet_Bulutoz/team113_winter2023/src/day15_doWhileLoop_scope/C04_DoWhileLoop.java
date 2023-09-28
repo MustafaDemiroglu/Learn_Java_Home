76b3cdfb3c461f14d7dbff2b0e41efa0fbaa68cc
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-09-28 19:58:17 +0200

    dowhile

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day15_doWhileLoop_scope/C04_DoWhileLoop.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day15_doWhileLoop_scope/C04_DoWhileLoop.java
@@ -1,5 +1,7 @@
 package day15_doWhileLoop_scope;
 
+import java.util.Scanner;
+
 public class C04_DoWhileLoop {
     public static void main(String[] args) {
         // Kullanicidan kullanmak üzere sayilar alin
@@ -7,6 +9,23 @@ public class C04_DoWhileLoop {
         // kullanici 0 a bastiginda
         // 0 haric kac sayi girdigini ve toplmlari yazdirin
 
-        
+        Scanner scan = new Scanner(System.in);
+
+        int girilenSayi = 1;
+        int toplam = 0;
+        int sayiAdedi= 0;
+
+        while (girilenSayi !=0) {
+            System.out.println("Toplanmak üzere tam sayi giriniz " +
+                    "\nBitirmek icin 0'a basiniz ");
+            girilenSayi = scan.nextInt();
+
+            if (girilenSayi != 0) {
+                toplam += girilenSayi;
+                sayiAdedi ++;
+            }
+        }
+
+        System.out.println("Girilen " + sayiAdedi + " adet sayinin toplamli " + toplam);
     }
 }
