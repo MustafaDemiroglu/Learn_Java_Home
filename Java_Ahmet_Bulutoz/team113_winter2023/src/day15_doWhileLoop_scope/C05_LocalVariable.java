95508bfa067eeb9a672dd2a14d5b9b1acdef8f86
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-10-04 16:26:06 +0200

    Create C05_LocalVariable.java

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day15_doWhileLoop_scope/C05_LocalVariable.java
@@ -0,0 +1,13 @@
+package day15_doWhileLoop_scope;
+
+public class C05_LocalVariable {
+    public static void main(String[] args) {
+        String isim = "Ali Can";
+    }
+
+    public static void baskaMethod(){
+        // System.out.println(isim);
+        int sayi = 5;
+    }
+
+}
