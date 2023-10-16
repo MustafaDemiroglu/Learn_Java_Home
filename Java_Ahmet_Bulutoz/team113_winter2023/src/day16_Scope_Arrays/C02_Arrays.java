3ae3172200976c7ac5b4001063a45e7fce166abc
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-10-16 22:14:28 +0200

    Create C02_Arrays.java

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day16_Scope_Arrays/C02_Arrays.java
@@ -0,0 +1,11 @@
+package day16_Scope_Arrays;
+
+public class C02_Arrays {
+    public static void main(String[] args) {
+        String[] isimler = {"Nuerfsan", "Berke" , "Mustafa"};
+        int[] sayilar = {3,4,5,6,7,8,9,10,33,66,6,6,3,3};
+        char karakterler[] = {'e', '4', ','};
+
+        // Array non-primitive data türlerindendir
+    }
+}
