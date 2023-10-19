f110837098003704c6c65592eb11e1f2a6f0d333
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-10-19 20:57:07 +0200

    arrays

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day17_arrays/C02_ArraydeIstenenElemaniBulma.java
@@ -0,0 +1,11 @@
+package day17_arrays;
+
+public class C02_ArraydeIstenenElemaniBulma {
+    public static void main(String[] args) {
+        // Soru 4 - Verilen bir array da istenen bir lemanin var oup olmadigini
+        // ve varsa kac kere kullanildigini yazdiran bir method olusturun
+
+
+
+    }
+}
