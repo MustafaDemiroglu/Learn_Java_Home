346349513492750ed6e1871a5f8389d27d29da0b
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-10-22 18:29:01 +0200

    arrays

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day17_arrays/C04_SagaKaydirma.java
@@ -0,0 +1,25 @@
+package day17_arrays;
+
+import java.util.Arrays;
+
+public class C04_SagaKaydirma {
+    public static void main(String[] args) {
+        // Veilen int bir array deki tüm elementleri bir saga kaydirip
+        // sondaki elementi de en basa alip kaydedin
+        // arn : 3,4,5,6   ==> son hali = 6,3,4,5
+
+        int [] arr = {3,4,5,6};
+
+        int temp = arr[arr.length-1];
+
+        for (int i = 0; i <arr.length-1 ; i++) {
+            arr[arr.length-1-i] = arr[arr.length-2-i];
+        }
+
+        arr[0] = temp;
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+}
