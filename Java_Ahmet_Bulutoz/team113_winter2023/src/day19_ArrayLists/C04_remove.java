e228d143882a11d4e127f89c67d5b4bf70d54445
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-11-01 21:13:22 +0100

    ArrayList

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day19_ArrayLists/C04_remove.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day19_ArrayLists/C04_remove.java
@@ -1,7 +1,29 @@
 package day19_ArrayLists;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class C04_remove {
     public static void main(String[] args) {
-        
+        List<String> harfler = new ArrayList<>();
+
+        harfler.add("A");
+        harfler.add("B");
+        harfler.add("K");
+        System.out.println(harfler);   // [A, B, K]
+
+        System.out.println(harfler.remove("B"));
+        System.out.println(harfler);    // [A, K]
+
+        System.out.println(harfler.remove(0));
+        System.out.println(harfler);    // [K]
+
+        List<Integer> sayilar = new ArrayList<>();
+        sayilar.add(1);
+        sayilar.add(3);
+        sayilar.add(18);
+        sayilar.add(2);
+
+
     }
 }
