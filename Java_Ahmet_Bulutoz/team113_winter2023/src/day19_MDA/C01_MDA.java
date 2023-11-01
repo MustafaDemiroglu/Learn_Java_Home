82e00eaf7751e94313d85801ac6debbc88c1a94c
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-11-01 16:47:00 +0100

    mda

---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day19_MDA/C01_MDA.java
@@ -0,0 +1,43 @@
+package day19_MDA;
+
+import java.util.Arrays;
+
+public class C01_MDA {
+    public static void main(String[] args) {
+
+        // Soru 2 - verilen 2 katli bir array de ayni index e
+        // sahip elementleri toplayip
+        // yeni olusturacagimiz tek katli
+        // bir array e bu toplamlari atayin
+        // input :      int[][] arr ={{3,4,5},{2.3.6.7}}
+        // output:      int[] arr2 ={5,7,11}
+
+        int[][] arr ={{3,4,5},{2,3,6,7}};
+
+        // ortak index kullanabilmek icin en kisa olani bulmaliyiz
+
+        int enKisaArrayLength = arr[0].length;       //3
+
+        for (int i = 0; i <arr.length ; i++) {
+            if (arr[i].length < enKisaArrayLength) {
+                enKisaArrayLength = arr[i].length;
+            }
+        }
+
+        // toplamlari koymak icin yeni bir array olusturalim
+
+        int[] toplamlarArrayi = new int[enKisaArrayLength];
+        int indekstekiElementlerToplami=0;
+
+        for (int i = 0; i < toplamlarArrayi.length ; i++) {
+            for (int j = 0; j <arr.length ; j++) {
+                indekstekiElementlerToplami += arr[j][i];
+            }
+            toplamlarArrayi[i]=indekstekiElementlerToplami;
+            indekstekiElementlerToplami=0;
+        }
+
+        System.out.println(Arrays.toString(toplamlarArrayi));
+    }
+
+}
