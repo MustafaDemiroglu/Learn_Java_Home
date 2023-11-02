ad59e95472f350ce0b0892c52fa517429e17e499
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-11-02 21:41:34 +0100

    ArrayListMethods

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day19_ArrayLists/C05_Tekrarlardankurtulma.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day19_ArrayLists/C05_Tekrarlardankurtulma.java
@@ -1,6 +1,7 @@
 package day19_ArrayLists;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class C05_Tekrarlardankurtulma {
@@ -18,11 +19,22 @@ public class C05_Tekrarlardankurtulma {
 
         List<Integer> benzersizElementlerListesi = new ArrayList<>();
         for (int i = 0; i <arr.length ; i++) {
-            if (!benzersizElementlerListesi.contains(i)) {
-                benzersizElementlerListesi.add(i);
+            if (!benzersizElementlerListesi.contains(arr[i])) {
+                benzersizElementlerListesi.add(arr[i]);
             }
         }
-        System.out.println(benzersizElementlerListesi); // 
+        System.out.println(benzersizElementlerListesi); // [3, 4, 5, 6, 2, 7]
+
+        // istenen listeyi list olarak elde ettik
+        // bunu array e direk olarak atayamayiz.
+
+        arr = new int[benzersizElementlerListesi.size()];
+        System.out.println(Arrays.toString(arr));   //  [0, 0, 0, 0, 0, 0]
+
+        for (int i = 0; i < arr.length ; i++) {
+            arr[i]=benzersizElementlerListesi.get(i);
+        }
+        System.out.println(Arrays.toString(arr));   // [3, 4, 5, 6, 2, 7]
 
 
     }
---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day19_ArrayLists/C07_subList.java
@@ -0,0 +1,4 @@
+package day19_ArrayLists;
+
+public class C07_subList {
+}
