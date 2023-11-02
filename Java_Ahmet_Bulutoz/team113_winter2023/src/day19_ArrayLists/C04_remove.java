3202be7eeefd3993a35842fff8922d69072daabf
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-11-02 19:34:02 +0100

    ArrayList

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day19_ArrayLists/C04_remove.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day19_ArrayLists/C04_remove.java
@@ -24,6 +24,24 @@ public class C04_remove {
         sayilar.add(18);
         sayilar.add(2);
 
+        System.out.println(sayilar);    // [1, 3, 18, 2]
+
+        /*
+            Eger sayilardan olusan bir list varsa
+            remove methoduna yazacagimiz sayiyi index olarak kabul eder
+         */
+
+        sayilar.remove(1);
+        System.out.println(sayilar);    // [1, 18, 2]
+
+        // 1 i silmek istersek. yani istedigimiz bir elementi
+
+        Integer silinecekSayi = 1;
+        sayilar.remove(silinecekSayi);
+        System.out.println(sayilar);    // [18, 2]
+
+
+
 
     }
 }
--- Java_Ahmet_Bulutoz/team113_winter2023/src/day19_ArrayLists/C05_Tekrarlardankurtulma.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day19_ArrayLists/C05_Tekrarlardankurtulma.java
@@ -1,10 +1,28 @@
 package day19_ArrayLists;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class C05_Tekrarlardankurtulma {
     public static void main(String[] args) {
 
         // verilen bir array den tekrar eden sayilari silip
         // her bir elementin sadece bir kez kullanildigi bir hale dönüstürün
+
+        int[] arr = {3,4,5,6,3,4,2,3,5,4,6,5,4,3,5,7};
+
+        // bir tabe bos list olsuturalim
+        // array deki her bir elemani ele alalim
+        // ele aldigimiz element list de yoksa ekleyelim, varsa eklemeyelim
+        // boylece bnzersiz elementlerden olsusan bir list olusturalim.
+
+        List<Integer> benzersizElementlerListesi = new ArrayList<>();
+        for (int i = 0; i <arr.length ; i++) {
+            if (!benzersizElementlerListesi.contains(i)) {
+                benzersizElementlerListesi.add(i);
+            }
+        }
+        System.out.println(benzersizElementlerListesi); // 
 
 
     }
