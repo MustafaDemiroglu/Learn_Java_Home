1a5844e168c01fb6168c1f98d59153c56ce5516a
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2023-11-05 10:44:13 +0100

    For Each loop

--- Java_Ahmet_Bulutoz/team113_winter2023/src/day21_arrayList_forEachLoop/C02_ForEachLoop.java
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day21_arrayList_forEachLoop/C02_ForEachLoop.java
@@ -25,10 +25,20 @@ public class C02_ForEachLoop {
         System.out.println("Elementler toplami : " + elementlerToplami);
 
         // arr in elementlerinden 3 ile bölünebilenleri yazdirin
-        int ucIleB
+        for (int each:arr) {
+            if (each %3 == 0) {
+                System.out.print(each + ", ");
+            }
+        }
+        System.out.println(" ");
 
         // arr in elementleri icinde kac tane tek sayi oldugunu bulun
-
-
+        int sayac = 0;
+        for (int each:arr) {
+            if (each %2 != 0) {
+                sayac++;
+            }
+        }
+        System.out.println("Array deki tek sayi adedi : " + sayac);
     }
 }
---
+++ Java_Ahmet_Bulutoz/team113_winter2023/src/day21_arrayList_forEachLoop/C03_ArrayElementleriniBenzersizYapma.java
@@ -0,0 +1,25 @@
+package day21_arrayList_forEachLoop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class C03_ArrayElementleriniBenzersizYapma {
+    public static void main(String[] args) {
+        // Verilen bir array de tekrar eden elementler icin
+        // mükerrer olanlari silip, tüm elemanlardan sadece 1 tane yapip
+        // eski array a yeni halini atayip yazdirin
+
+        int[] arr ={3,5,6,7,3,2,3,5,8,7,1,2,3,4,5,8};
+
+        List<Integer> benzersizElementListesi = new ArrayList<>();
+        // array deki herbir elementi tek tek kontrol edelim
+        // List de yoksa ekleyelim, varsa eklemeyelim
+
+        for (int each:arr) {
+            if (!benzersizElementListesi.contains(each)) {
+                benzersizElementListesi.add(each);
+            }
+        }
+        System.out.println(benzersizElementListesi);
+    }
+}
